dc2df094bfa58efec9910e6818839de9f63d4db0
Marlon Kuzmick <mkuzmick@gmail.com>
2024-05-18 11:17:27 -0400

    Updates:

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'emilys-candy': ['var(--font-emilys-candy)'], // Add this line
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
